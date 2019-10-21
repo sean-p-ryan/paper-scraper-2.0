32952af5a8f246e0ebd64275c886ef293b1ce84d
Sean Ryan <seanpryan89@gmail.com>
2019-10-20 22:31:36 -0400

    post request body not appearing

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,58 @@
-const paperURLS = document.querySelectorAll('.paper-url');
+const paperURLS = document.querySelectorAll('.paper-url');;
 const table = document.querySelector(".papers-table");
 const addRowButton = document.querySelector(".add-row-button");
 const deleteRowButton = document.querySelector(".delete-row-button");
+const submitButton = document.querySelector(".submit-button");
 
 const newRowMarkup = `
     <td>
-        <input type="text" name="paperUrl" placeholder="Paper URL">
+        <input type="text" class="paper-url" placeholder="Paper URL">
     </td>
     <td class="paperTitle"></td>
     <td class="Authors"></td>
     <td class="journalName"></td>
 `
 
-
-const getAllPapers = async () => {
-    const response = await fetch('/papers/all');
-    const json = await response.json();
-    const shows = json.results;
-    console.log(shows);
-    getShowNames(shows);
-}
-
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function(e) {
     if (e.target === addRowButton) {
         addNewRow();
     } else if (e.target === deleteRowButton) {
         deleteLastRow();
+    } else if (e.target === submitButton) {
+        sendURLsToServer()
     }
 })
+
+const sendURLsToServer = () => {
+    const urlArray = "test"
+
+    fetch('data/retrieve', {
+            method: 'POST',
+            headers: new Headers(),
+            body: { "paperURLs": "test" }
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+}
+
+const getPaperURLs = () => {
+    let urlArray = [];
+    paperURLS.forEach(paper => {
+        urlArray.push(paper.value);
+    })
+    return urlArray;
+};
 
 const addNewRow = () => {
     const newRow = document.createElement('tr');
     document.body.appendChild(newRow);
-    newRow.innerHTML = newRowMarkup;    
+    newRow.innerHTML = newRowMarkup;
     table.appendChild(newRow);
 }
 
 const deleteLastRow = () => {
     const rows = document.getElementsByTagName('tr');
-    const lastRow = rows[rows.length-1];
+    const lastRow = rows[rows.length - 1];
     table.removeChild(lastRow);
-}
-
+};
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+const connect = require('connect')
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const http = require('http');
+const bodyParser = require('body-parser')
 
-const base = 'https://api.themoviedb.org/3';
-const api_key = process.env.API_KEY;
-require('dotenv').config();
 
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
@@ -25,18 +24,18 @@ function normalizePort(val) {
     return false;
 }
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
 server.on("listening", () => {
     console.log(`server is listening for requests on port ${server.address().port}`);
 });
 
 
-// app.listen(3000, () => console.log('listening on port 3000!'));
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
 
 // Define routes
-
-// app.get('/tv/popular', async(req, res) => {
-
-// });
-
+app.post('/data/retrieve', (req, res) => {
+    console.log(req);
+});
